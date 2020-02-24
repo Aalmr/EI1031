b0aadeeb8a8a65197074abea51e7871f774e199e
almar <almar.b.miller@gmail.com>
2020-02-24 18:09:35 +0100

    assert equals

--- src/test/java/ListDividerTest.java
+++ src/test/java/ListDividerTest.java
@@ -7,41 +7,39 @@ import org.junit.runners.Parameterized.Parameters;
 import java.util.Arrays;
 import java.util.Collection;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 @RunWith(Parameterized.class)
 public class ListDividerTest {
-    private Object[]  elementos;
-    private Object[]  fraccion;
-    private Object[] list1;
-    private Object[]  list2;
+    private int[]  elementos;
+    private float  fraccion;
+    private int[][] lists;
     private static ListDivider listDivider;
 
-    public ListDividerTest(Object[] elementos, Object[] fraccion, Object[] list1, Object[] list2){
+    public ListDividerTest(int[] elementos, float fraccion, int[][] lists){
         this.elementos = elementos;
         this.fraccion = fraccion;
-        this.list1 = list1;
-        this.list2 = list2;
+        this.lists = lists;
     }
 
     @Parameters
-    public static Collection<Object[][]> data(){
-        return (Collection<Object[][]>) Arrays.asList(new Object[][][]{
-                {{2,2,-3}, {0.5}, {-3,2}, {2}},
-                {{2,2,3}, {1}, {2,2,3}, {}},
-                {{3,4,3,4}, {0.45}, {3,4}, {3,4}},
-                {{2,-1,2,3}, {0.4}, {2}, {-1,2,3}},
-                {{1,2,2,3}, {0}, {}, {1,2,2,3}},
-                {{2,2,-6,-6,3,3}, {0.5}, {-6,2,3}, {-6,2,3}},
-                {{}, {0.467}, null, null},
-                {null, {0.32}, null, null},
-                {{9,-6,9,4}, null, null, null},
-                {{3,7,5,2,2,5}, {-3.2}, null, null},
-                {{7,7,7,8,8}, {1.1}, null, null},
-                {{6,6,6,6,6}, {0.8}, {6,6,6,6}, {6}},
-                {{5,4,2,9,6,7,1}, {0.6}, {1,2,4,5,6,7,9}, {}},
-                {{0,3,8,1,7,4}, {0.4}, {}, {0,1,2,3,4,7,8}},
-                {{-3,-8,-8,-1,-3}, {0.3}, {-8,-3}, {-8,-3,-1}},
+    public static Collection<Object[]> data(){
+        return (Collection<Object[]>) Arrays.asList(new Object[][]{
+                {new int[]{2,2,-3}, (float) 0.5, new int[][]{{-3,2}, {2}}},
+                {new int[]{2,2,3}, (float) 1, new int[][]{{2,2,3}, {}}},
+                {new int[]{3,4,3,4}, (float)0.45, new int[][]{{3,4}, {3,4}}},
+                {new int[]{2,-1,2,3}, (float)0.4, new int[][]{{2}, {-1,2,3}}},
+                {new int[]{1,2,2,3}, (float)0, new int[][]{{}, {1,2,2,3}}},
+                {new int[]{2,2,-6,-6,3,3}, (float)0.5, new int[][]{{-6,2,3}, {-6,2,3}}},
+                {new int[]{}, (float)0.467, null},
+                {null, (float)0.32, null},
+                {new int[]{3,7,5,2,2,5}, (float)-3.2, null},
+                {new int[]{7,7,7,8,8}, (float)1.1, null},
+                {new int[]{6,6,6,6,6}, (float)0.8, new int[][]{{6,6,6,6}, {6}}},
+                {new int[]{5,4,2,9,6,7,1}, (float)0.6, new int[][]{{1,2,4,5,6,7,9}, {}}},
+                {new int[]{0,3,8,1,7,4}, (float)0.4, new int[][]{{}, {0,1,2,3,4,7,8}}},
+                {new int[]{-3,-8,-8,-1,-3}, (float)0.3, new int[][]{{-8,-3}, {-8,-3,-1}}},
         });
     }
 
@@ -52,6 +50,6 @@ public class ListDividerTest {
 
     @Test
     public void divideList() {
-        fail("No implementado");
+        assertEquals(lists, listDivider.divideList(elementos, fraccion));
     }
 }
